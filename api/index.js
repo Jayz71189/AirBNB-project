9ec1aedc97958e9fda6bbc9a263e410e829c08fc
Jason <jayz71189@gmail.com>
2024-10-30 12:33:01 -0400

    first

--- api/index.js
+++ api/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 
-router.post("/", async (req, res) => {
+// app.get("/", async (req, res) => {
+//     let { firstName, lastName, email, username}
+
+app.post("/", async (req, res) => {
   let { firstName, lastName, email, username } = req.body;
 
   const user = await User.create({
